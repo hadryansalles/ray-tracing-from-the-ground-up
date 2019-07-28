e003ed4806a6b7d2e0566ef65710636a82131643
hadryans <hadryansalles00@gmail.com>
2019-07-28 20:28:32 -0300

    Maths refactored

--- source/Utilities/Maths.hpp
+++ source/Utilities/Maths.hpp
@@ -1,13 +1,6 @@
-#ifndef __MATHS__
-#define __MATHS__
+#pragma once
 
-inline double
-max(double x0, double x1);
-
-inline double
-max(double x0, double x1)
-{
+double max(double x0, double x1) {
 	return((x0 > x1) ? x0 : x1);
 }
 
-#endif
