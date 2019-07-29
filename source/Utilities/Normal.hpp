4b1353c30fd92720868a1f8afc2c21e516607589
hadryans <hadryansalles00@gmail.com>
2019-07-28 21:13:29 -0300

    Point3D refactored

--- source/Utilities/Normal.hpp
+++ source/Utilities/Normal.hpp
@@ -27,3 +27,9 @@ public:
 	Normal operator* (const double a) const;			// multiplication by a double on the right			
 	void normalize(void); 								// convert normal to a unit normal
 };
+
+Normal operator* (const Matrix& mat, const Normal& n);
+double operator* (const Vector3D& v, const Normal& n);
+Vector3D operator- (const Vector3D&, const Normal& n);
+Vector3D operator+ (const Vector3D& v, const Normal& n);
+Normal operator* (const double a, const Normal& n);
--- source/Utilities/Point3D.cpp
+++ source/Utilities/Point3D.cpp
@@ -1,46 +1,25 @@
-// this file contains the definition of the class Point3D
-
-#include <math.h>
 #include "Point3D.hpp"
-
-
-// --------------------------------------------- default constructor
 
 Point3D::Point3D()
 	:x(0), y(0), z(0)
 {}
 
-
-// --------------------------------------------- constructor
-
 Point3D::Point3D(const double a)
 	:x(a), y(a), z(a)
 {}
-
-// --------------------------------------------- constructor
 
 Point3D::Point3D(const double a, const double b, const double c)
 	:x(a), y(b), z(c)
 {}
 
-
-// --------------------------------------------- copy constructor
-
 Point3D::Point3D(const Point3D& p)
 	:x(p.x), y(p.y), z(p.z)
 {}
 
-
-// --------------------------------------------- destructor
-
 Point3D::~Point3D() 
 {}
 
-
-// --------------------------------------------- assignment operator
-
-Point3D& 
-Point3D::operator= (const Point3D& rhs) {
+Point3D& Point3D::operator= (const Point3D& rhs) {
 	
 	if (this == &rhs)
 		return (*this);
@@ -50,28 +29,49 @@ Point3D::operator= (const Point3D& rhs) {
 	return (*this);
 }
 
-
-
-// --------------------------------------------- distance
 // distance between two points
-
-double
-Point3D::distance(const Point3D& p) const {
+double Point3D::distance(const Point3D& p) const {
 	return (sqrt(		(x - p.x) * (x - p.x) 
 					+ 	(y - p.y) * (y - p.y)
 					+	(z - p.z) * (z - p.z) ));
 }
 
-
-// non-member function
-
-// --------------------------------------------- operator*
 // multiplication by a matrix on the left
-
-Point3D 						
-operator* (const Matrix& mat, const Point3D& p) {
+Point3D operator* (const Matrix& mat, const Point3D& p) {
 	return (Point3D(mat.m[0][0] * p.x + mat.m[0][1] * p.y + mat.m[0][2] * p.z + mat.m[0][3],
 					mat.m[1][0] * p.x + mat.m[1][1] * p.y + mat.m[1][2] * p.z + mat.m[1][3],
 					mat.m[2][0] * p.x + mat.m[2][1] * p.y + mat.m[2][2] * p.z + mat.m[2][3]));
 }
 
+// unary minus
+Point3D Point3D::operator- (void) const {
+	return (Point3D(-x, -y, -z));
+}
+// the vector that joins two points
+Vector3D Point3D::operator- (const Point3D& p) const {
+	return (Vector3D(x - p.x,y - p.y,z - p.z));
+}
+// addition of a vector to a point that returns a new point
+Point3D Point3D::operator+ (const Vector3D& v) const {
+	return (Point3D(x + v.x, y + v.y, z + v.z));
+}
+// subtraction of a vector from a point that returns a new point
+Point3D Point3D::operator- (const Vector3D& v) const {
+	return (Point3D(x - v.x, y - v.y, z - v.z));
+}
+// mutliplication by a double on the right
+Point3D Point3D::operator* (const double a) const {
+	return (Point3D(x * a,y * a,z * a));
+}
+
+// the square of the distance between the two points as a member function
+double Point3D::d_squared(const Point3D& p) const {
+	return (	(x - p.x) * (x - p.x) 
+			+ 	(y - p.y) * (y - p.y)
+			+	(z - p.z) * (z - p.z) );
+}
+
+// multiplication by a double on the left
+Point3D operator* (double a, const Point3D& p) {
+	return (Point3D(a * p.x, a * p.y, a * p.z));
+}
