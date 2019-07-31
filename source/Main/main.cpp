56820c4004bbb5f98d0526cf38e945c83b16e7c3
hadryans <hadryansalles00@gmail.com>
2019-07-31 10:32:17 -0300

    render from camera

--- source/Main/main.cpp
+++ source/Main/main.cpp
@@ -5,9 +5,13 @@ int main(){
     w.build();
     if(w.tracer_ptr == NULL){
         printf("Tracer null. Aborting.\n");
-        return 0;
+        return 1;
     }
-    w.render_scene();
+    if(w.camera == NULL){
+        printf("Camera null. Aborting.\n");
+        return 1;
+    }
+    w.camera->render_scene(w);
     printf("Main thread terminated.\n");
     return 0;
 }
