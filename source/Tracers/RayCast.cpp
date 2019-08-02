99c2a6baca1fe34088690132ca1b9b303610789f
hadryans <hadryansalles00@gmail.com>
2019-08-02 02:22:36 -0300

    RayCast tracer class

--- source/Tracers/RayCast.cpp
+++ source/Tracers/RayCast.cpp
@@ -1,11 +1,36 @@
 #include "RayCast.hpp"
 #include "../World/World.hpp"
+#include "../Utilities/ShadeRec.hpp"
+#include "../Materials/Material.hpp"
 
 RayCast::RayCast(World* world_ptr_r):
-    Tracer(world_ptr)
+    Tracer(world_ptr_r)
 {}
 
+RGBColor RayCast::trace_ray(const Ray& ray) const{
+    ShadeRec sr(world_ptr->hit_objects(ray));
+    if(sr.hit_an_object){
+        sr.ray = ray;
+        return (sr.material_ptr->shade(sr));
+    }
+    else{
+        return (world_ptr->background_color);
+    }
+}
+
 RGBColor RayCast::trace_ray(const Ray ray, const int depth) const{
+    ShadeRec sr(world_ptr->hit_objects(ray));
+
+    if(sr.hit_an_object){
+        sr.ray = ray;
+        return (sr.material_ptr->shade(sr));
+    }
+    else{
+        return (world_ptr->background_color);
+    }
+}
+
+RGBColor RayCast::trace_ray(const Ray ray, float& tmin, const int depth) const{
     ShadeRec sr(world_ptr->hit_objects(ray));
 
     if(sr.hit_an_object){
--- source/Tracers/RayCast.hpp
+++ source/Tracers/RayCast.hpp
@@ -6,5 +6,7 @@ class RayCast: public Tracer{
 public:
     RayCast(World* world_ptr_ = NULL);
 
-    virtual RGBColor trace_ray(const Ray ray, const int depth) const;
+	virtual RGBColor trace_ray(const Ray& ray) const;
+	virtual RGBColor trace_ray(const Ray ray, const int depth) const;
+	virtual RGBColor trace_ray(const Ray ray, float& tmin, const int depth) const;
 };
