cf9e4ce8519009a6874a2b70809d60894a8bc109
hadryans <hadryansalles00@gmail.com>
2019-08-04 05:28:25 -0300

    min/max floats

--- source/Utilities/Maths.hpp
+++ source/Utilities/Maths.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-inline double max(double x0, double x1) {
+inline float max(float x0, float x1) {
 	return((x0 > x1) ? x0 : x1);
+}
+
+inline float min(float a, float b){
+	return (a > b)? a : b;
 }
 
 inline float rand_float(){
