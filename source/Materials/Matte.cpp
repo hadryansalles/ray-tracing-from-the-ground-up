6552628ca24ead6e5b361a8bbceb2dc7d1bdf677
hadryans <hadryansalles00@gmail.com>
2019-08-08 09:55:42 -0300

    shadow shade with matte

--- source/Materials/Matte.cpp
+++ source/Materials/Matte.cpp
@@ -107,8 +107,15 @@ RGBColor Matte::shade(ShadeRec& sr){
     for(int j = 0; j < num_lights; j++){
         Vector3D wi = sr.w.lights[j]->get_direction(sr);
         float ndotwi = sr.normal*wi;
+        bool in_shadow = false;
         if(ndotwi > 0.0){
-            L += diffuse_brdf->f(sr, wo, wi)*sr.w.lights[j]->L(sr)*ndotwi;
+            if(sr.w.lights[j]->get_shadows()){
+                Ray shadowRay(sr.hit_point, wi);
+                in_shadow = sr.w.lights[j]->in_shadow(shadowRay, sr);
+            }
+            if(!in_shadow) {
+                L += diffuse_brdf->f(sr, wo, wi)*sr.w.lights[j]->L(sr)*ndotwi;
+            }
         }
     }
     return L;
